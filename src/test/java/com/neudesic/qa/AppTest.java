5a18da0cfc017724e2567c56a558183bc1973168
Narayanan Kangath <Narayanan.Kangath@neudesic.com>
2025-04-22 22:07:24 +0530

    change browser to run headless

--- src/test/java/com/neudesic/qa/AppTest.java
+++ src/test/java/com/neudesic/qa/AppTest.java
@@ -20,7 +20,7 @@ public class AppTest {
     @BeforeAll
     static void launchBrowser() {
         playwright = Playwright.create();
-        browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false).setSlowMo(50));
+        browser = playwright.chromium().launch();
     }
 
     @AfterAll
